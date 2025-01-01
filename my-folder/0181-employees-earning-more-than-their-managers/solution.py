03528e4a218f917d9c885846202ac54f96c72e0b
Junaid Khan <JKHAN.JK2920@GMAIL.COM>
2025-01-01 08:55:23 +0000

    [LeetCode Sync] Runtime - 285 ms (94.65%), Memory - 68.3 MB (21.49%)

---
+++ my-folder/0181-employees-earning-more-than-their-managers/solution.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def find_employees(employee: pd.DataFrame) -> pd.DataFrame:
+    df = employee.merge(employee,how='inner',left_on = 'managerId',right_on = 'id')
+
+    emp = df[df['salary_x'] > df['salary_y']]['name_x']
+
+    return pd.DataFrame({'Employee' : emp})
